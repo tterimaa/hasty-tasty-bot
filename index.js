f4bce2a48f8a54ff554eeaf2347cd2c1d79cf366
tterimaa <tterimaa@gmail.com>
2020-11-07 19:43:16 +0200

    Call python process

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const cp = require('child_process');
 const groupService = require('./groupService');
 const pollService = require('./pollService');
 
@@ -31,6 +32,19 @@ bot.onText(/\/done/, (msg) => {
   pollService.sendPolls(msg.chat.id, bot);
 });
 
+const callPythonService = async (input) => {
+  let dataToSend;
+  const python = cp.spawn('python', ['script.py', input]);
+  python.stdout.on('data', (data) => {
+    console.log('Pipe data from python script');
+    dataToSend = data.toString();
+  });
+  python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    console.log(dataToSend);
+  });
+};
+
 bot.on('poll_answer', async (answer) => {
   const poll = pollService.openPolls[answer.poll_id];
   pollService.updatePoll(answer);
@@ -41,6 +55,7 @@ bot.on('poll_answer', async (answer) => {
   if (pollService.isEverybodyAnswered(poll.chat_id, group.getSize())) {
     console.log('all answers received');
     console.group(group);
+    callPythonService(JSON.stringify(group.users));
     pollService.deletePollsByChat(poll.chat_id);
   }
   console.log(pollService.openPolls);
